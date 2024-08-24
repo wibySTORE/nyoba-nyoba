49f11e5b2ef5ba3eedfaf23e600021ab89660264
wiby <muhamadtama619@gmail.com>
2024-08-24 08:51:20 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,302 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+            <title>Pastelink.ID - Best Pastelink Service</title>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="87ND0Ziur7oHEGqCW6ipAkT7bwwG2IVI2sSLE5rH">
+    <meta name="robots" content="max-image-preview:large" />
+
+    <meta name="google-site-verification" content="EJnxqa6FRKVU6fmvFWZgij4siwOm8JdNks863nzLMEM" />
+
+    <meta name="theme-color" content="#2563eb" />
+    <meta name="msapplication-navbutton-color" content="#2563eb" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="#2563eb" />
+    <meta name="apple-mobile-web-app-capable" content="true" />
+
+    <meta name="msvalidate.01" />
+    <link rel="canonical" href="https://pastelink.id" />
+    <link rel="apple-touch-icon" sizes="120x120" href="https://pastelink.id/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="152x152" href="https://pastelink.id/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="https://pastelink.id/favicon.ico" />
+    <link rel="shortcut icon" type="image/x-icon" href="https://pastelink.id/favicon.ico" />
+
+            <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://pastelink.id" />
+        <meta property="og:site_name" content="PasteLink.ID" />
+        <meta property="og:description"
+            content="Temukan kemudahan membuat dan mempublikasikan teks dengan tautan di Pastelink! Sederhana dan handal, gabungkan informasi dengan mudah dalam satu tempat. Singkatkan tautan Anda untuk kemudahan berbagi. Jelajahi Pastelink - tempat terbaik untuk menggabungkan dan membagikan tautan dan teks dengan tampilan yang bersih dan menarik." />
+        <meta property="og:image:alt" content="PasteLink.ID" />
+        <meta property="og:image" content="https://pastelink.id/img/icon-500.png" />
+
+        <meta name="twitter:title" content="PasteLink.ID" />
+        <meta name="twitter:url" content="https://pastelink.id" />
+        <meta name="twitter:description"
+            content="Temukan kemudahan membuat dan mempublikasikan teks dengan tautan di Pastelink! Sederhana dan handal, gabungkan informasi dengan mudah dalam satu tempat. Singkatkan tautan Anda untuk kemudahan berbagi. Jelajahi Pastelink - tempat terbaik untuk menggabungkan dan membagikan tautan dan teks dengan tampilan yang bersih dan menarik." />
+        <meta name="twitter:card" content="PasteLink.ID" />
+        <meta name="twitter:image:alt" content="PasteLink.ID" />
+        <meta name="twitter:image:src" content="https://pastelink.id/img/icon-500.png" />
+    
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=nunito:200,300,400,500,600,700,800,900&display=swap"
+        rel="stylesheet" />
+
+    <!-- Scripts -->
+    
+    <link rel="preload" as="style" href="https://pastelink.id/build/assets/app-B9mNS6DM.css" />
+    <link rel="modulepreload" href="https://pastelink.id/build/assets/app-Bsl5FIOY.js" />
+    <link rel="stylesheet" href="https://pastelink.id/build/assets/app-B9mNS6DM.css" />
+    <script type="module" src="https://pastelink.id/build/assets/app-Bsl5FIOY.js"></script>
+
+    
+</head>
+
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <nav x-data="{ open: false }"
+    class="sticky top-0 z-10 bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+    <!-- Primary Navigation Menu -->
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 xl:px-12">
+        <div class="flex justify-between h-16">
+            <div class="flex">
+                <!-- Logo -->
+                <div class="shrink-0 flex items-center">
+                    <a href="https://pastelink.id">
+                        <img class="block h-9 w-auto" src="https://pastelink.id/favicon.ico"
+    alt="PasteLink.ID">
+                    </a>
+                </div>
+
+                <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <a class="inline-flex items-center px-1 pt-1 border-b-2 border-primary-400 dark:border-primary-600 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-primary-700 transition duration-150 ease-in-out" href="https://pastelink.id">
+    Home
+</a>
+
+                    
+                    
+                </div>
+            </div>
+
+                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <a class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out" href="https://pastelink.id/login">
+    Login
+</a>
+
+                    <a class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out" href="https://pastelink.id/register">
+    Register
+</a>
+                </div>
+            
+            <!-- Hamburger -->
+            <div class="-me-2 flex items-center sm:hidden">
+                <button @click="open = ! open"
+                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
+                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
+                            stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+    </div>
+
+    <!-- Responsive Navigation Menu -->
+    <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
+        <div class="pt-2 pb-3 space-y-1">
+            <a class="block w-full ps-3 pe-4 py-2 border-l-4 border-primary-400 dark:border-primary-600 text-start text-base font-medium text-primary-700 dark:text-primary-300 bg-primary-50 dark:bg-primary-900/50 focus:outline-none focus:text-primary-800 dark:focus:text-primary-200 focus:bg-primary-100 dark:focus:bg-primary-900 focus:border-primary-700 dark:focus:border-primary-300 transition duration-150 ease-in-out" href="https://pastelink.id">
+    Home
+</a>
+
+                    </div>
+
+        <!-- Responsive Settings Options -->
+        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
+                            <div class="pt-2 pb-3 space-y-1">
+                    <a class="block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 dark:hover:border-gray-600 focus:outline-none focus:text-gray-800 dark:focus:text-gray-200 focus:bg-gray-50 dark:focus:bg-gray-700 focus:border-gray-300 dark:focus:border-gray-600 transition duration-150 ease-in-out" href="https://pastelink.id/login">
+    Login
+</a>
+
+                    <a class="block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 dark:hover:border-gray-600 focus:outline-none focus:text-gray-800 dark:focus:text-gray-200 focus:bg-gray-50 dark:focus:bg-gray-700 focus:border-gray-300 dark:focus:border-gray-600 transition duration-150 ease-in-out" href="https://pastelink.id/register">
+    Register
+</a>
+                </div>
+                    </div>
+    </div>
+</nav>
+
+        <!-- Page Heading -->
+                    <header class="mb-12 bg-white dark:bg-gray-800 shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 xl:px-12">
+                    <h2 class="text-gray-800 dark:text-gray-200">
+            <span class="font-bold text-xl leading-tight">Pastelink.ID</span>
+            <span class="mx-0.5 font-light text-xs opacity-85">Best Pastelink Service</span>
+        </h2>
+                </div>
+            </header>
+        
+        <!-- Page Content -->
+        <main>
+            <div class="-mt-12 sm:mt-0 sm:mb-12 max-w-7xl mx-auto sm:px-6 lg:px-8 xl:px-12">
+            <div class="bg-primary-600 text-white px-10 py-20 sm:rounded-xl text-center">
+                <h2 class="mb-10 text-4xl font-bold">Welcome to Pastelink.ID, layanan pastelink terbaik!</h2>
+                <span>#1 Layanan Pastelink di Indonesia.</span>
+            </div>
+        </div>
+    
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 xl:px-12">
+    <div class="bg-white text-gray-700 shadow-sm border border-gray-200 rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-700">
+    <div class="px-6 pt-6 flex flex-col gap-y-0.5"><h2 class="text-center text-primary-500 font-bold">
+                    CREAT, EASY-PEASY!
+                </h2>
+
+                <h3 class="text-center text-4xl font-bold">
+                    Create a new Paste
+                </h3></div>
+
+            <div class="p-6">
+    <form id="fm_link" method="post" action="https://pastelink.id/api/links" class="mt-6">
+                    <input type="hidden" name="_token" value="87ND0Ziur7oHEGqCW6ipAkT7bwwG2IVI2sSLE5rH" autocomplete="off">
+                    <div class="lg:flex lg:*:flex-grow lg:gap-6">
+                        <div class="space-y-6">
+                            <div>
+                                <label class="block font-medium text-sm text-gray-700 dark:text-gray-300" for="link-title">
+    Enter your title
+</label>
+                                <input   class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-primary-500 dark:focus:border-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 rounded-md shadow-sm mt-1 block w-full" id="link-title" name="title" type="text" autocomplete="title" placeholder="Place a title here, no links">
+                                                            </div>
+
+                            <div>
+                                <label class="block font-medium text-sm text-gray-700 dark:text-gray-300" for="link-content">
+    Enter your text or link
+</label>
+                                <textarea  
+    class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-primary-500 dark:focus:border-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 rounded-md shadow-sm mt-1 block w-full" id="link-content" name="content" rows="10" required="required" autocomplete="content" placeholder="Enter the main text of your paste"></textarea>
+                                                            </div>
+                        </div>
+
+                        <div class="space-y-6 mt-6 lg:mt-0">
+                            <div x-data="{ pass: false }">
+                                <div class="block font-medium text-sm text-gray-700 dark:text-gray-300">Visibility</div>
+
+                                <div class="mt-1 flex gap-3 *:flex-grow">
+                                    <label class="cursor-pointer">
+                                        <input type="radio" name="visibility" value="" class="sr-only peer"
+                                            x-on:click="pass = false" checked />
+                                        <span
+                                            class="block text-center px-5 py-2 border border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 rounded-md shadow-sm peer-checked:text-white peer-checked:bg-primary-600 peer-checked:border-primary-600">Public</span>
+                                    </label>
+                                    <label class="cursor-pointer">
+                                        <input type="radio" name="visibility" value="true" class="sr-only peer"
+                                            x-on:click="pass = true"  />
+                                        <span
+                                            class="block text-center px-5 py-2 border border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 rounded-md shadow-sm peer-checked:text-white peer-checked:bg-primary-600 peer-checked:border-primary-600">Private</span>
+                                    </label>
+                                </div>
+
+                                <div class="mt-5" :class="pass ? '' : 'hidden'">
+                                    <label class="block font-medium text-sm text-gray-700 dark:text-gray-300" for="link-pass">
+    Password
+</label>
+                                    <input   class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-primary-500 dark:focus:border-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 rounded-md shadow-sm mt-1 block w-full" id="link-pass" name="pass" type="text" placeholder="Access password">
+                                                                    </div>
+                            </div>
+
+                            <div x-data="{ exp: true }">
+                                <div class="block font-medium text-sm text-gray-700 dark:text-gray-300">Expiry</div>
+
+                                <div class="mt-1 flex gap-3 *:flex-grow">
+                                    <label class="cursor-pointer">
+                                        <input type="radio" name="expiry" value="" class="sr-only peer"
+                                            x-on:click="exp = false" />
+                                        <span
+                                            class="block text-center px-5 py-2 border border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 rounded-md shadow-sm peer-checked:text-white peer-checked:bg-primary-600 peer-checked:border-primary-600">Never</span>
+                                    </label>
+                                    <label class="cursor-pointer">
+                                        <input type="radio" name="expiry" value="true" class="sr-only peer"
+                                            x-on:click="exp = true" checked />
+                                        <span
+                                            class="block text-center px-5 py-2 border border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 rounded-md shadow-sm peer-checked:text-white peer-checked:bg-primary-600 peer-checked:border-primary-600">Expired</span>
+                                    </label>
+                                </div>
+
+                                <div x-data="{ minDate: new Date().toISOString().split('T')[0] }" class="mt-5" :class="exp ? '' : 'hidden'">
+                                    <label class="block font-medium text-sm text-gray-700 dark:text-gray-300" for="link-exp">
+    Expired
+</label>
+                                    <input   class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-primary-500 dark:focus:border-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 rounded-md shadow-sm mt-1 block w-full" id="link-exp" name="expired" type="date" x-bind:min="minDate">
+                                                                    </div>
+                            </div>
+
+                            <div>
+                                <div class="block font-medium text-sm text-gray-700 dark:text-gray-300">Style</div>
+
+                                <div class="mt-1 flex gap-5">
+                                    <label class="inline-flex gap-2 items-center cursor-pointer">
+                                        <input type="radio" name="style" id="style01" value="A"
+                                            checked>
+                                        <span>Default</span>
+                                    </label>
+
+                                    <label class="inline-flex gap-2 items-center cursor-pointer">
+                                        <input type="radio" name="style" id="style02" value="B"
+                                            >
+                                        <span>Flat</span>
+                                    </label>
+
+                                    <label class="inline-flex gap-2 items-center cursor-pointer">
+                                        <input type="radio" name="style" id="style03" value="C"
+                                            >
+                                        <span>Neumorphism</span>
+                                    </label>
+                                </div>
+                            </div>
+
+                            <div class="flex items-center gap-4">
+                                <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+    Publish
+</button>
+
+                                                            </div>
+
+                                                    </div>
+                    </div>
+                </form>
+</div>
+</div>
+</div>
+        </main>
+
+        <footer class="mt-12 bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700 dark:text-white">
+    <div class="px-4 py-4 text-center">©2024 — Made with. <a class="font-semibold hover:underline"
+            href="https://www.pastelink.id">PasteLink.ID</a></div>
+</footer>
+    </div>
+
+            <script>
+            (() => {
+                document.querySelectorAll(".copy_link").forEach((e) => {
+                    e.addEventListener("click", () => {
+                        if (!e.classList.contains("is-copied")) {
+                            const l = location.origin + "/" + e.dataset.id;
+                            cpyTC(l);
+                            e.classList.add("is-copied");
+                            setTimeout(() => {
+                                e.classList.remove("is-copied");
+                            }, 2000);
+                        }
+                    });
+                });
+            })();
+        </script>
+    </body>
+
+</html>
